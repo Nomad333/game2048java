d26e5e36ffe3c6f85145bf022f701808243c23c1
nomad333 <zanrjnbr3@gmail.com>
2026-02-16 21:50:14 +0200

    Better ConsoleCell.java

--- src/com/game2048/cell/ConsoleCell.java
+++ src/com/game2048/cell/ConsoleCell.java
@@ -3,32 +3,51 @@ package com.game2048.cell;
 
 import com.game2048.consoleobj.ConsoleObject;
 
+import java.security.InvalidParameterException;
+
 //┏━━━┓
 //┃ x ┃
 //┗━━━┛
 
-public class ConsoleCell extends ConsoleObject{
+public class ConsoleCell extends ConsoleObject {
     private int number;
+    private int numWidth;
 
     public ConsoleCell(int number) {
         super();
-        setNumber(number);
+        this.number = number;
+        setNumWidth((number + "").length());
+    }
+
+    public ConsoleCell(int number, int numWidth) {
+        super();
+        this.number = number;
+        setNumWidth(numWidth);
+    }
+
+    public int getNumWidth() {
+        return numWidth;
+    }
+
+    public void setNumWidth(int numWidth) {
+        if ((number + "").length() > numWidth) throw new InvalidParameterException("numWidth");
+        this.numWidth = numWidth;
+        reDraw();
+    }
+
+    public void setNumber(int number, boolean autoWidth) {
+        this.number = number;
+        if (autoWidth) setNumWidth((number + "").length());
+        reDraw();
     }
 
     public int getNumber() {
         return number;
     }
 
-    public void setNumber(int number) {
-        this.number = number;
-        int repeatCount = (number+"").length();
-        header = "┏━━" + "━".repeat(repeatCount) + "┓";
-        body = "┃ %d ┃".formatted(number);
-        footer = "┗━━" + "━".repeat(repeatCount) + "┛";
-    }
-    public void setNumSize(int repeatCount) {
-        header = "┏━━" + "━".repeat(repeatCount) + "┓";
-        body = "┃ %d ".formatted(number) +" ".repeat(repeatCount-(number+"").length()) + "┃";
-        footer = "┗━━" + "━".repeat(repeatCount) + "┛";
+    private void reDraw() {
+        header = "┏━━" + "━".repeat(numWidth) + "┓";
+        body = "┃ %d ".formatted(number) + " ".repeat(numWidth - (number + "").length()) + "┃";
+        footer = "┗━━" + "━".repeat(numWidth) + "┛";
     }
 }
--- src/com/game2048/main/Main.java
+++ src/com/game2048/main/Main.java
@@ -1,27 +1,27 @@
 package com.game2048.main;
 
 import com.game2048.cell.ConsoleCell;
-import com.game2048.color.StringWrapper;
 import com.game2048.color.ConsoleColor;
+import com.game2048.color.StringWrapper;
 
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        ConsoleCell cell = new ConsoleCell(50000);
         List<ConsoleCell> cells = List.of(
                 new ConsoleCell(5),
                 new ConsoleCell(5),
-        new ConsoleCell(5),
+                new ConsoleCell(5),
                 new ConsoleCell(5)
         );
 
-        cells.get(0).setNumSize(5);
-        cells.get(1).setNumSize(50);
+        cells.get(0).setNumWidth(50);
+        cells.get(0).setNumber(1000000, false);
+        cells.get(1).setNumWidth(12);
 
-        for (int i = 0; i < 3; i++){
+        for (int i = 0; i < 3; i++) {
             for (var el : cells) {
-                System.out.print(StringWrapper.str(el.next(), ConsoleColor.BLUE));
+                System.out.print(StringWrapper.str(el.next(), ConsoleColor.values()[i + 5 % 3]));
             }
             System.out.println();
         }
